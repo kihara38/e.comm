d49f119d7f88791787a63f96525af1233b3f3d90
kihara Nelson <rickymburu38@gmail.com>
2022-09-24 07:55:23 +0300

    starting Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-
-
+import {BrowserRouter as  Router, Route, Routes} from 'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container} from 'react-bootstrap';
+import Homescreen from "./screens/Homescreen";
+import ProductScreen from './screens/ProductScreen';
 const App=()=> {
   return (
+    <Router>
     <>
-      <h1>Welcome to E.comm</h1>
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <Routes>
+        <Route path='/' element={<Homescreen/>} exact />
+        <Route path='/product/:id' element={<ProductScreen/>} exact />
+        </Routes>
+      </Container>
+      
+    </main>
+    <Footer/>
     </>
+    </Router>
   );
 }
 
